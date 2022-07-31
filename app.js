1b13d6b10dbbc1873676d0afd55275728c26f877
Gaston Amigo <gastonamigo@gmail.com>
2022-07-31 19:59:25 -0300

    se agrego el ejercicio completo

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-let base = prompt("Ingrese el numero");
-let exponente = prompt("Ingrese la potencia");
+let productos = prompt("Ingrese la cantidad de productos");
+
+let cantidadProductos = parseInt(productos);
+
+if (cantidadProductos > 0) {
+
+  costoEnvio(cantidadProductos);
+
+  function costoEnvio(cantidad) {
+    
+    let valorEnvio1 = 450;
+    let valorEnvio2 = 650;
+    let total=0;
+    for (let i = 1; i <= cantidad; i++) {
+      let alto = parseInt(prompt("Ingrese Alto del paquete N°"+i+": "));
+      let ancho = parseInt(prompt("Ingrese Ancho del paquete N°"+i+": "));
+      let largo = parseInt(prompt("Ingrese largo del paquete N°"+i+": "));
 
 
-let numero = parseInt(base);
-let potencia = parseInt(exponente);
-let resultado = numero;
-if (potencia >= 0){
-  
-  for (let i = 1; i < potencia ;i++) {
-  
-    resultado = numero * resultado;  
+      if ((alto * ancho * largo) <= 6000) {
 
-  console.log(resultado);
-}
-alert("El resultado es: " + resultado);
-}else{
-    alert("solo se admiten potencias positivas")
+        total = total + valorEnvio1;
+
+      } else if ((alto * ancho * largo) > 6000) {
+        total = total + valorEnvio2;
+
+      }
+      
+    }
+    alert("El Costo de envio por todos los productos es: $" + total);
+
+
+  }
+
+
+
+} else {
+  alert("Ingrese al menos un producto")
 }
 
